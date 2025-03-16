d8c1a74500612b2d17fd96956b701e70dc48ee44
Hariteja82 <141202181+Hariteja82@users.noreply.github.com>
2025-03-16 18:48:41 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull hariteja82/simple-python-flask-app:latest
+sudo docker pull hariteja82/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -itd --name app -p 5000:5000 hariteja82/simple-python-flask-app:latest
+sudo docker run -itd --name app -p 5000:5000 hariteja82/simple-python-flask-app:latest
